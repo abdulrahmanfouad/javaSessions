620433ca6e7fed9722649977dcaae7a11f40c985
unknown <abdulrahmanfouad549@gmail.com>
2023-03-28 22:41:12 +0200

    Creating Java Sessions

---
+++ src/concatenatingTwoChars.java
@@ -0,0 +1,12 @@
+public class concatenatingTwoChars {
+    public static void main(String[] args) {
+        //example of documentation comment
+        /**
+         * Example of Documentation Comment
+         */
+        char x = 's';
+        char a = 't';
+        System.out.print(x);
+        System.out.print(a);
+    }
+}
---
+++ src/printingConstantValue.java
@@ -0,0 +1,11 @@
+public class printingConstantValue {
+    public static void main(String[] args) {
+ /*       final double constant1=1.4;
+        final float constant2 = 1.5f;
+//        constant1=2.5;
+        System.out.println(constant1);
+        System.out.println(constant2);
+
+  */
+    }
+}
